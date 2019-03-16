6731c8809a19fb3b404b30a8faed8654ee70ce99
Alessandro Genova <ales.genova@gmail.com>
2019-03-15 22:45:56 -0400

    Add McLeod detector

--- src/lib.rs
+++ src/lib.rs
@@ -95,57 +95,148 @@ impl<T> PitchDetector<T> for AutocorrelationDetector<T>
     fn get_pitch(&mut self, signal: &[T], sample_rate: usize, power_threshold: T, clarity_threshold: T) -> Option<Pitch<T>> {
         assert_eq!(signal.len(), self.internals.size);
 
+        if get_power_level(signal) < power_threshold {
+            return None;
+        }
+
         let (signal_complex, rest) = self.internals.complex_buffers.split_first_mut().unwrap();
         let (scratch, _) = rest.split_first_mut().unwrap();
         let (autocorr, _) = self.internals.real_buffers.split_first_mut().unwrap();
 
-        copy_real_to_complex(signal, signal_complex, ComplexComponent::Re);
-        autocorrelation(signal_complex, scratch, autocorr);
-
-        if autocorr[0] / T::from_usize(signal.len()).unwrap() < power_threshold {
-            return None;
-        }
-
+        autocorrelation(signal, signal_complex, scratch, autocorr);
         let clarity_threshold = clarity_threshold * autocorr[0];
-        let peaks = detect_peaks(autocorr);
-        let chosen_peak = choose_peak(&peaks, clarity_threshold);
-        let chosen_peak = match chosen_peak {
-            Some(peak) => {
-                 Some(correct_peak(peak, autocorr, PeakCorrection::None))
-            },
-            None => {
-                None
-            }
-        };
 
-        let pitch = match chosen_peak {
-            Some(peak) => {
-                let frequency = T::from_usize(sample_rate).unwrap() / peak.0;
-                let clarity = peak.1 / autocorr[0];
-                Some(Pitch { frequency, clarity })
-            },
-            None => {
-                None
-            }
-        };
-        pitch
+        pitch_from_peaks(autocorr, sample_rate, clarity_threshold, PeakCorrection::None)
     }
 }
 
-fn autocorrelation<T>(signal: &mut [Complex<T>], scratch: &mut [Complex<T>], result: &mut [T])
+pub struct McLeodDetector<T>
     where T : FloatCore
 {
+    internals: DetectorInternals<T>
+}
+
+impl<T> McLeodDetector<T>
+    where T: FloatCore
+{
+    pub fn new(size: usize, padding: usize) -> Self {
+        let internals = DetectorInternals::new(2, 2, size, padding);
+        McLeodDetector {
+            internals
+        }
+    }
+}
+
+impl<T> PitchDetector<T> for McLeodDetector<T>
+    where T : FloatCore
+{
+    fn get_pitch(&mut self, signal: &[T], sample_rate: usize, power_threshold: T, clarity_threshold: T) -> Option<Pitch<T>> {
+        assert_eq!(signal.len(), self.internals.size);
+
+        if get_power_level(signal) < power_threshold {
+            return None;
+        }
+
+        let (signal_complex, rest) = self.internals.complex_buffers.split_first_mut().unwrap();
+        let (scratch0, _) = rest.split_first_mut().unwrap();
+        let (scratch1, rest) = self.internals.real_buffers.split_first_mut().unwrap();
+        let (nsdf, _) = rest.split_first_mut().unwrap();
+
+        normalized_square_difference(signal, signal_complex, scratch0, scratch1, nsdf);
+
+        pitch_from_peaks(nsdf, sample_rate, clarity_threshold, PeakCorrection::None)
+    }
+}
+
+fn pitch_from_peaks<T>(input: &[T], sample_rate: usize, clarity_threshold: T, correction: PeakCorrection) -> Option<Pitch<T>>
+    where T: FloatCore
+{
+    let peaks = detect_peaks(input);
+    let chosen_peak = choose_peak(&peaks, clarity_threshold);
+    let chosen_peak = match chosen_peak {
+        Some(peak) => {
+                Some(correct_peak(peak, input, correction))
+        },
+        None => {
+            None
+        }
+    };
+
+    let pitch = match chosen_peak {
+        Some(peak) => {
+            let frequency = T::from_usize(sample_rate).unwrap() / peak.0;
+            let clarity = peak.1 / input[0];
+            Some(Pitch { frequency, clarity })
+        },
+        None => {
+            None
+        }
+    };
+    pitch
+}
+
+fn get_power_level<T>(signal: &[T]) -> T
+    where T : FloatCore
+{
+    let mut power = T::zero();
+    for i in 0..signal.len() {
+        power = power + signal[i] * signal[i];
+    }
+    power
+}
+
+fn autocorrelation<T>(signal: &[T], signal_complex: &mut [Complex<T>], scratch: &mut [Complex<T>], result: &mut [T])
+    where T : FloatCore
+{
+    copy_real_to_complex(signal, signal_complex, ComplexComponent::Re);
     let mut planner = FFTplanner::new(false);
-    let fft = planner.plan_fft(signal.len());
-    fft.process(signal, scratch);
+    let fft = planner.plan_fft(signal_complex.len());
+    fft.process(signal_complex, scratch);
     for i in 0..scratch.len() {
         scratch[i].re = scratch[i].re * scratch[i].re + scratch[i].im * scratch[i].im;
         scratch[i].im = T::zero();
     }
     let mut planner = FFTplanner::new(true);
-    let inv_fft = planner.plan_fft(signal.len());
-    inv_fft.process(scratch, signal);
-    copy_complex_to_real(signal, result, ComplexComponent::Re);
+    let inv_fft = planner.plan_fft(signal_complex.len());
+    inv_fft.process(scratch, signal_complex);
+    copy_complex_to_real(signal_complex, result, ComplexComponent::Re);
+}
+
+fn m_of_tau<T>(signal: &[T], signal_square_sum: Option<T>, result: &mut [T])
+    where T : FloatCore
+{
+    assert!(result.len() >= signal.len());
+
+    let signal_square_sum = match signal_square_sum {
+        Some(val) => val,
+        None => {
+            let mut val = T::zero();
+            for i in 0..signal.len() {
+                val = val + signal[i] * signal[i];
+            }
+            val
+        }
+    };
+
+    result[0] = T::from_usize(2).unwrap() * signal_square_sum;
+    for i in 1..signal.len() {
+        result[i] = result[i - 1] - signal[i - 1] * signal[i - 1];
+    }
+
+    // Signal has no padding, but result does
+    for i in signal.len()..result.len() {
+        result[i] = result[i - 1];
+    }
+}
+
+fn normalized_square_difference<T>(signal: &[T], scratch0: &mut [Complex<T>], scratch1: &mut [Complex<T>], scratch2: &mut [T], result: &mut [T])
+    where T : FloatCore
+{
+    autocorrelation(signal, scratch0, scratch1, result);
+    m_of_tau(signal, Some(result[0]), scratch2);
+    for i in 0..result.len() {
+        result[i] = T::from_usize(2).unwrap() * result[i] / scratch2[i];
+    }
 }
 
 fn detect_crossings<T: FloatCore>(arr: &[T]) -> Vec<(usize, usize)> {
@@ -280,6 +371,27 @@ fn copy_complex_to_real<T: FloatCore>(input: &[Complex<T>], output: &mut [T], co
 mod tests {
     use super::*;
 
+    #[test]
+    fn peak_correction() {
+        let point = quadratic_interpolation(
+            Point{x: -1.5, y: - (1.5 * 1.5) + 4.0},
+            Point{x: -0.5, y: - (0.5 * 0.5) + 4.0},
+            Point{x: 0.5, y: - (0.5 * 0.5) + 4.0}
+        );
+        assert_eq!(point.x, 0.0);
+        assert_eq!(point.y, 4.0);
+    }
+
+    #[test]
+    fn autocorrelation_pure_frequency() {
+        pure_frequency(String::from("Autocorrelation"));
+    }
+
+    #[test]
+    fn mcleod_pure_frequency() {
+        pure_frequency(String::from("McLeod"));
+    }
+
     fn get_chunk<T: FloatCore>(signal: &[T], start: usize, window: usize, output: &mut [T]) {
         let start = match signal.len() > start {
             true => start,
@@ -312,19 +424,22 @@ mod tests {
         signal
     }
 
-    #[test]
-    fn peak_correction() {
-        let point = quadratic_interpolation(
-            Point{x: -1.5, y: - (1.5 * 1.5) + 4.0},
-            Point{x: -0.5, y: - (0.5 * 0.5) + 4.0},
-            Point{x: 0.5, y: - (0.5 * 0.5) + 4.0}
-        );
-        assert_eq!(point.x, 0.0);
-        assert_eq!(point.y, 4.0);
+    fn detector_factory(name: String, window: usize, padding: usize) -> Box<PitchDetector<f64>> {
+        match name.as_ref() {
+            "McLeod" => {
+                return Box::new(AutocorrelationDetector::<f64>::new(window, padding));
+            },
+            "Autocorrelation" => {
+                return Box::new(AutocorrelationDetector::<f64>::new(window, padding));
+            },
+            _ => {
+                panic!("Unknown detector {}", name);
+            }
+        }
+        
     }
 
-    #[test]
-    fn pure_frequency() {
+    fn pure_frequency(detector_name: String) {
         const SAMPLE_RATE : usize = 48000;
         const FREQUENCY : f64 = 440.0;
         const DURATION : f64 = 4.0;
@@ -340,7 +455,7 @@ mod tests {
 
         let mut chunk = new_real_buffer(WINDOW);
 
-        let mut detector = AutocorrelationDetector::<f64>::new(WINDOW, PADDING);
+        let mut detector = detector_factory(detector_name, WINDOW, PADDING);
 
         for i in 0..N_WINDOWS {
             let t : usize = i * DELTA_T;
