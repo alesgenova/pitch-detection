515e60d99828beea4a6696bfeb101ff13ee7a2b8
Alessandro Genova <ales.genova@gmail.com>
2020-12-19 12:45:41 -0500

    Cargo format a few remaining files

--- src/detector/mod.rs
+++ src/detector/mod.rs
@@ -1,12 +1,19 @@
-use crate::float::Float;
 use crate::detector::internals::Pitch;
+use crate::float::Float;
 
-pub mod internals;
 pub mod autocorrelation;
+pub mod internals;
 pub mod mcleod;
 
 pub trait PitchDetector<T>
-    where T : Float
+where
+    T: Float,
 {
-    fn get_pitch(&mut self, signal: &[T], sample_rate: usize, power_threshold: T, clarity_threshold: T) -> Option<Pitch<T>>;
+    fn get_pitch(
+        &mut self,
+        signal: &[T],
+        sample_rate: usize,
+        power_threshold: T,
+        clarity_threshold: T,
+    ) -> Option<Pitch<T>>;
 }
