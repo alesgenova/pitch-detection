38e13142c5c9ca5c544dd8c86ee01e6521a2df7e
Jason Siefken <siefkenj@gmail.com>
2021-02-27 22:47:50 -0500

    Document quadtradic peak finding

--- src/float/mod.rs
+++ src/float/mod.rs
@@ -1,7 +1,8 @@
 use num_traits::float::FloatCore as NumFloatCore;
 use rustfft::FFTnum;
+use std::fmt::Display;
 
-pub trait Float: NumFloatCore + FFTnum {}
+pub trait Float: Display + NumFloatCore + FFTnum {}
 
 impl Float for f64 {}
 impl Float for f32 {}
--- src/utils/peak.rs
+++ src/utils/peak.rs
@@ -5,11 +5,6 @@ pub enum PeakCorrection {
     None,
 }
 
-struct Point<T: Float> {
-    x: T,
-    y: T,
-}
-
 fn detect_crossings<'a, T: Float>(arr: &'a [T]) -> impl Iterator<Item = (usize, usize)> + 'a {
     arr.windows(2)
         .enumerate()
@@ -61,36 +56,45 @@ pub fn correct_peak<T: Float>(peak: (usize, T), data: &[T], correction: PeakCorr
     match correction {
         PeakCorrection::Quadratic => {
             let idx = peak.0;
-            let point = quadratic_interpolation(
-                Point {
-                    x: T::from_usize(idx - 1).unwrap(),
-                    y: data[idx - 1],
-                },
-                Point {
-                    x: T::from_usize(idx).unwrap(),
-                    y: data[idx],
-                },
-                Point {
-                    x: T::from_usize(idx + 1).unwrap(),
-                    y: data[idx + 1],
-                },
-            );
-            (point.x, point.y)
+            let (x, y) = find_quadratic_peak(data[idx - 1], data[idx], data[idx + 1]);
+            (x + T::from_usize(idx).unwrap(), y)
         }
         PeakCorrection::None => (T::from_usize(peak.0).unwrap(), peak.1),
     }
 }
 
-fn quadratic_interpolation<T: Float>(
-    left: Point<T>,
-    center: Point<T>,
-    right: Point<T>,
-) -> Point<T> {
-    let shift = T::from_f64(0.5).unwrap() * (right.y - left.y)
-        / (T::from_f64(2.0).unwrap() * center.y - left.y - right.y);
-    let x = center.x + shift;
-    let y = center.y + T::from_f64(0.25).unwrap() * (right.y - left.y) * shift;
-    Point { x, y }
+/// Use a quadratic interpolation to find the maximum of
+/// a parabola passing through `(-1, y0)`, `(0, y1)`, `(1, y2)`.
+///
+/// The output is of the form `(x-offset, peak value)`.
+fn find_quadratic_peak<T: Float>(y0: T, y1: T, y2: T) -> (T, T) {
+    // The quadratic ax^2+bx+c passing through
+    // (-1, y0), (0, y1), (1, y2), the
+    // has coefficients
+    //
+    // a = y0/2 - y1 + y2/2
+    // b = (y2 - y0)/2
+    // c = y1
+    //
+    // and a maximum at x=-b/(2a) and y=-b^2/(4a) + c
+
+    // Some constants
+    let two = T::from_f64(2.).unwrap();
+    let four = T::from_f64(4.).unwrap();
+
+    let a = (y0 + y2) / two - y1;
+    let b = (y2 - y0) / two;
+    let c = y1;
+
+    // If we're concave up, the maximum is at one of the end points
+    if a > T::zero() {
+        if y0 > y2 {
+            return (-T::one(), y0);
+        }
+        return (T::one(), y2);
+    }
+
+    (-b / (two * a), -b * b / (four * a) + c)
 }
 
 #[cfg(test)]
@@ -99,21 +103,34 @@ mod tests {
 
     #[test]
     fn peak_correction() {
-        let point = quadratic_interpolation(
-            Point {
-                x: -1.5,
-                y: -(1.5 * 1.5) + 4.0,
-            },
-            Point {
-                x: -0.5,
-                y: -(0.5 * 0.5) + 4.0,
-            },
-            Point {
-                x: 0.5,
-                y: -(0.5 * 0.5) + 4.0,
-            },
-        );
-        assert_eq!(point.x, 0.0);
-        assert_eq!(point.y, 4.0);
+        fn quad1(x: f64) -> f64 {
+            -x * x + 4.0
+        }
+        let (x, y) = find_quadratic_peak(quad1(-1.5), quad1(-0.5), quad1(0.5));
+        assert_eq!(x - 0.5, 0.0);
+        assert_eq!(y, 4.0);
+        let (x, y) = find_quadratic_peak(quad1(-3.5), quad1(-2.5), quad1(-1.5));
+        assert_eq!(x - 2.5, 0.0);
+        assert_eq!(y, 4.0);
+
+        fn quad2(x: f64) -> f64 {
+            -2. * x * x + 3. * x - 2.5
+        }
+
+        let (x, y) = find_quadratic_peak(quad2(-1.), quad2(0.), quad2(1.));
+        assert_eq!(x, 0.75);
+        assert_eq!(y, -1.375);
+
+        // Test off concave-up parabolas
+        fn quad3(x: f64) -> f64 {
+            x * x + 2.0
+        }
+
+        let (x, y) = find_quadratic_peak(quad3(0.), quad3(1.), quad3(2.));
+        assert_eq!(x + 1., 2.);
+        assert_eq!(y, 6.);
+        let (x, y) = find_quadratic_peak(quad3(-2.), quad3(-1.), quad3(0.));
+        assert_eq!(x - 1., -2.);
+        assert_eq!(y, 6.);
     }
 }
